6d3cb9d1635a980bfae0b1f59d03e70b1c1a3691
korotkov <korotkov@gosniias.ru>
2023-01-18 14:09:37 +0300

    New reliable inference interface

--- datasets.py
+++ datasets.py
@@ -2,14 +2,17 @@ from torch.utils.data import Dataset
 from glob import glob
 import numpy as np
 from PIL import Image
+import os
+from torch import FloatTensor
 
 class PowerlinesDataset(Dataset):
     'Custom primitive Dataset class for powerlines detection task'
     ''''''
-    def __init__(self ,train=True):
+    def __init__(self ,train=True, data_path = None, test=False):
         super().__init__()
         self.train = train
-        self.data = glob(f"data/images/{'train' if train else 'val'}/*")
+        self.test = test
+        self.data = glob(f"data/images/{'train' if train else 'val'}/*" if data_path is None else f"{data_path}/*") 
         self.labels = [i.replace('images', 'labels').replace('jpeg', 'txt') for i in self.data]
         
         
@@ -20,6 +23,10 @@ class PowerlinesDataset(Dataset):
         img = Image.open(self.data[idx]).convert("RGB")
         img = np.asarray(img).astype(np.float32)
         img /= 255.
+        
+        if self.test:
+            return FloatTensor(img.transpose(2, 0, 1)[None, ...]), -1
+            
         labels = np.loadtxt(self.labels[idx])
         c, x, y, w, h = labels
         x1 = x - w/2
@@ -33,3 +40,5 @@ class PowerlinesDataset(Dataset):
         
         return img.transpose(2, 0, 1), np.array([c, x1, y1, x2, y2]).astype(np.float32)
     
+    def getname(self, idx):
+        return os.path.basename(self.data[idx])
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from torchvision.ops import nms
 
 
 def get_targets(boxes):
@@ -30,3 +31,123 @@ def visualize(dataset, i=5):
     plt.axis('off')
     print(__)
     plt.show()
+    
+def IOU(boxA, boxB):
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+	# return the intersection over union value
+	return iou    
+    
+    
+def NMS_bad(outputs, conf_thresh, iou_thresh):
+    
+    output_nms = [] 
+    
+    for output in outputs:
+        
+        good_boxes = {'boxes': [], 'labels': [], 'scores': []}
+        
+        mask = output['scores'] > conf_thresh
+        
+        'if there are no boxes, skip'
+        if sum(mask) == 0:
+            continue
+        
+        boxes = output['boxes'][mask]
+        labels = output['labels'][mask]
+        scores = output['scores'][mask]
+        
+        'if there is only one box, leave it'
+        if sum(mask) == 1:
+            for key, arr in zip(good_boxes.keys(), [boxes, labels, scores]):
+                good_boxes[key].append(arr[0])
+            continue
+        
+        'check every pair of remaining boxes'
+        for i in range(len(boxes) - 1):
+            for j in range(i + 1, len(boxes)):
+                'skip different labels'
+                if labels[i] != labels[j]:
+                    continue
+                
+                iou = IOU(boxes[i], boxes[j])
+                if iou < iou_thresh:
+                    for key, arr in zip(good_boxes.keys(), [boxes, labels, scores]):
+                        good_boxes[key].append(arr[i])
+                        good_boxes[key].append(arr[j])
+                else:
+                    k = max(i, j, key=lambda x: scores[x])
+                    for key, arr in zip(good_boxes.keys(), [boxes, labels, scores]):
+                        good_boxes[key].append(arr[k])
+                    
+        output_nms.append(good_boxes)
+        
+    return output_nms
+       
+       
+def NMS(outputs, conf_thresh, iou_thresh):
+    ''''''''
+    'Input: list of dictionaries, each with "boxes", "scores", "labels"'
+    'Output: list of filtered dictionaries'
+    
+    supressed_outputs = []
+    if not isinstance(outputs, list) and isinstance(outputs, dict):
+        outputs = [outputs]
+    
+    for output in outputs:
+        
+        assert set(output.keys()) == set(['boxes', 'scores', 'labels'])
+        
+        mask = output['scores'] >= conf_thresh
+        
+        boxes = output['boxes'][mask]
+        scores = output['scores'][mask]
+        labels = output['labels'][mask]
+        
+        filtered_idxs = nms(boxes, scores, iou_threshold=iou_thresh)
+        
+        boxes = boxes[filtered_idxs]
+        scores = scores[filtered_idxs]
+        labels = labels[filtered_idxs]
+        supressed_outputs.append(
+            {
+                'boxes': boxes,
+                'labels': labels,
+                'scores': scores,
+            }
+        )
+    
+    return supressed_outputs
+        
+def save_result(img, result, img_path):
+    
+    my_dpi = 96 
+    fig, ax = plt.subplots(1,figsize=(1980/my_dpi, 1080/my_dpi))
+    ax.imshow(img.detach().cpu().numpy().transpose((1,2,0)))
+    
+    for box, label, score in zip(*result.values()):
+        x1, y1, x2, y2 = box.detach().cpu().numpy()
+        w = x2 - x1
+        h = y2 - y1
+        bbox = patches.Rectangle((x1,y1), w , h ,
+                    linewidth=2, edgecolor='g', facecolor='none', label=f'{int(label)} | {round(score.item(), 2)}')
+        ax.add_patch(bbox)
+    plt.axis('off')
+    plt.legend()
+    fig.savefig(img_path, dpi=fig.dpi,bbox_inches='tight')
+        
+
+    
